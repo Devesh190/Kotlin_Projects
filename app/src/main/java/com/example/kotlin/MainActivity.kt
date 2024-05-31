26efa333ff309c62e7b9e2ed010268b2312eda2e
Devesh00001 <deveshchoudhary373@gmail.com>
2024-05-31 11:41:27 +0530

    Add images for dice

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.kotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
+    private lateinit var iv:ImageView
     private lateinit var tv:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.mb_roll)
-        tv = findViewById(R.id.tv)
-
+        iv = findViewById(R.id.iv_dice)
+        tv = findViewById(R.id.tv_text)
         btn.setOnClickListener{ rollDice()}
 
     }
@@ -25,5 +29,14 @@ class MainActivity : AppCompatActivity(){
     private fun rollDice() {
         var randomNum = (1..6).random()
         tv.text = randomNum.toString()
+        when(randomNum.toString()){
+            "1" -> iv.setImageResource(R.drawable.dice_1)
+            "2" -> iv.setImageResource(R.drawable.dice_2)
+            "3" -> iv.setImageResource(R.drawable.dice_3)
+            "4" -> iv.setImageResource(R.drawable.dice_4)
+            "5" -> iv.setImageResource(R.drawable.dice_5)
+            "6" -> iv.setImageResource(R.drawable.dice_6)
+            else -> iv.setImageResource(R.drawable.empty_dice)
+        }
     }
 }
